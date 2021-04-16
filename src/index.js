b78e02d8dca7309853ab0d5b9f5d664c0ba7ebfe
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-16 20:40:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  
+
     const input = document.getElementById('input');
     input.addEventListener('click', function(event) {
     document.getElementById('tasks').innerHTML+=document.getElementById('new-task-description').value;
