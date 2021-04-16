e6e02f74dd310eb3be90bad34bf38e369300d003
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-16 20:18:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    let description=document.getElementById('new-task-description').value;
+    const input = document.getElementByType('submit');
+    input.addEventListener('submit', function(event) {
+    let todo=document.getElementById('lists').getElementsById('tasks');
+    todo.createElement('li').innertext=description;
+
+    });
+
+
+
+
 });
