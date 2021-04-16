6700e33200d89d98d0e3b51fe95869621d9e701f
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-16 20:37:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
     let description=document.getElementById('new-task-description').value;
-    const input = document.getElementByType('submit');
+    const input = document.getElementById('input');
     input.addEventListener('click', function(event) {
     document.getElementById('tasks').innerHTML=description;
     event.preventDefault();
